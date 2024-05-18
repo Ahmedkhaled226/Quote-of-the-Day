21cd0ad03c8801361a8989d338cbd1873bbb9b6f
Ahmedkhaled226 <168454962+Ahmedkhaled226@users.noreply.github.com>
2024-05-19 01:03:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+
+const mediumParagraphs = [
+    {
+        'author': '--Jim Rohn',
+        'quote': '"Beware of what you become in pursuit of what you want."'
+    },
+    {
+        'author': '--Epictetus',
+        'quote': '"It\'s not what happens to you, but how you react to it that matters."'
+    },
+    {
+        'author': '--Frank Sinatra',
+        'quote': '"The best revenge is massive success."'
+    },
+    {
+        'author': '--Wayne Gretzy',
+        'quote': '"You miss 100% of the shots you don\'t take."'
+    },
+    {
+        'author': '--Nelson Mandela',
+        'quote': '"Resentment is like drinking poison and waiting for your enemies to die."'
+    },
+    {
+        'author': '"--Elbert Hubbard"',
+        'quote': '"Do not take life too seriously. You will not get out alive."'
+    },
+
+];
+
+
+
+
+
+let previousIndex;
+let randomIndex;
+
+function generateParagraph() {
+    const paragraphElement = document.getElementById("quoteOutput");
+    const paragraphElement2 = document.getElementById("authorOutput");
+
+
+    randomIndex = Math.floor(Math.random() * mediumParagraphs.length);
+    do {
+        randomIndex = Math.floor(Math.random() * mediumParagraphs.length);
+    } while (randomIndex === previousIndex);
+
+
+    previousIndex = randomIndex;
+    paragraphElement.textContent = mediumParagraphs[randomIndex].quote;
+    paragraphElement2.textContent = mediumParagraphs[randomIndex].author;
+}
+
+
